303c408b8efb0463a69c66701c9767fc0dcd12f3
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-03-26 10:06:07 +0300

    initial

--- App.tsx
+++ App.tsx
@@ -1,118 +1,228 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  CameraRoll,
+  PhotoIdentifier,
+} from '@react-native-camera-roll/camera-roll';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  Alert,
+  FlatList,
+  Image,
+  Linking,
+  Platform,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
 } from 'react-native';
+import Permissions, {PERMISSIONS} from 'react-native-permissions';
+import {ShimmerView} from './src/components';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App: React.FC = () => {
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [photos, setPhotos] = useState<PhotoIdentifier[]>([]);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const openSettingsAlert = useCallback(({title}: {title: string}) => {
+    Alert.alert(title, '', [
+      {
+        isPreferred: true,
+        style: 'default',
+        text: 'Open Settings',
+        onPress: () => Linking?.openSettings(),
+      },
+      {
+        isPreferred: false,
+        style: 'destructive',
+        text: 'Cancel',
+        onPress: () => {},
+      },
+    ]);
+  }, []);
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const checkAndroidPermissions = useCallback(async () => {
+    if (parseInt(Platform.Version as string, 10) >= 33) {
+      const permissions = await Permissions.checkMultiple([
+        PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
+        PERMISSIONS.ANDROID.READ_MEDIA_VIDEO,
+      ]);
+      if (
+        permissions[PERMISSIONS.ANDROID.READ_MEDIA_IMAGES] ===
+          Permissions.RESULTS.GRANTED &&
+        permissions[PERMISSIONS.ANDROID.READ_MEDIA_VIDEO] ===
+          Permissions.RESULTS.GRANTED
+      ) {
+        setHasPermission(true);
+        return;
+      }
+      const res = await Permissions.requestMultiple([
+        PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
+        PERMISSIONS.ANDROID.READ_MEDIA_VIDEO,
+      ]);
+      if (
+        res[PERMISSIONS.ANDROID.READ_MEDIA_IMAGES] ===
+          Permissions.RESULTS.GRANTED &&
+        res[PERMISSIONS.ANDROID.READ_MEDIA_VIDEO] ===
+          Permissions.RESULTS.GRANTED
+      ) {
+        setHasPermission(true);
+      }
+      if (
+        res[PERMISSIONS.ANDROID.READ_MEDIA_IMAGES] ===
+          Permissions.RESULTS.DENIED ||
+        res[PERMISSIONS.ANDROID.READ_MEDIA_VIDEO] === Permissions.RESULTS.DENIED
+      ) {
+        checkAndroidPermissions();
+      }
+      if (
+        res[PERMISSIONS.ANDROID.READ_MEDIA_IMAGES] ===
+          Permissions.RESULTS.BLOCKED ||
+        res[PERMISSIONS.ANDROID.READ_MEDIA_VIDEO] ===
+          Permissions.RESULTS.BLOCKED
+      ) {
+        openSettingsAlert({
+          title: 'Please allow access to your photos and videos from settings',
+        });
+      }
+    } else {
+      const permission = await Permissions.check(
+        PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+      );
+      if (permission === Permissions.RESULTS.GRANTED) {
+        setHasPermission(true);
+        return;
+      }
+      const res = await Permissions.request(
+        PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+      );
+      if (res === Permissions.RESULTS.GRANTED) {
+        setHasPermission(true);
+      }
+      if (res === Permissions.RESULTS.DENIED) {
+        checkAndroidPermissions();
+      }
+      if (res === Permissions.RESULTS.BLOCKED) {
+        openSettingsAlert({
+          title: 'Please allow access to the photo library from settings',
+        });
+      }
+    }
+  }, [openSettingsAlert]);
+
+  const checkPermission = useCallback(async () => {
+    if (Platform.OS === 'ios') {
+      const permission = await Permissions.check(PERMISSIONS.IOS.PHOTO_LIBRARY);
+      if (
+        permission === Permissions.RESULTS.GRANTED ||
+        permission === Permissions.RESULTS.LIMITED
+      ) {
+        setHasPermission(true);
+        return;
+      }
+      const res = await Permissions.request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+      if (
+        res === Permissions.RESULTS.GRANTED ||
+        res === Permissions.RESULTS.LIMITED
+      ) {
+        setHasPermission(true);
+      }
+      if (res === Permissions.RESULTS.BLOCKED) {
+        openSettingsAlert({
+          title: 'Please allow access to the photo library from settings',
+        });
+      }
+    } else if (Platform.OS === 'android') {
+      checkAndroidPermissions();
+    }
+  }, [checkAndroidPermissions, openSettingsAlert]);
+
+  useEffect(() => {
+    checkPermission();
+  }, [checkPermission]);
+
+  const fetchPhotos = useCallback(async () => {
+    const res = await CameraRoll.getPhotos({
+      first: 10,
+      assetType: 'Photos',
+    });
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    setPhotos(res?.edges);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (hasPermission) {
+      fetchPhotos();
+    }
+  }, [hasPermission, fetchPhotos]);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const savePhoto = useCallback(() => {
+    Alert.alert('Do you want to save this image?', '', [
+      {
+        isPreferred: true,
+        text: 'Yes',
+        onPress: async () => {
+          const res = await CameraRoll.save(
+            'https://fastly.picsum.photos/id/400/2440/1400.jpg?hmac=qSYbRbCQhkj1aMCy6AEHbsrcbXalaPHr6hPCPdxlO5o',
+          );
+          console.log(res);
+          if (res) {
+            Alert.alert('Image saved');
+          }
+        },
+        style: 'default',
+      },
+      {
+        isPreferred: false,
+        text: 'No',
+        onPress: () => {},
+        style: 'destructive',
+      },
+    ]);
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView>
+      <FlatList
+        numColumns={3}
+        data={isLoading ? Array(15).fill('') : photos}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({item, index}) => {
+          if (isLoading) {
+            return (
+              <ShimmerView key={index} delay={index * 100} width={'33%'} />
+            );
+          }
+          return (
+            <Image
+              key={item?.node?.image?.uri}
+              source={{uri: item?.node?.image?.uri}}
+              height={140}
+              style={styles.image}
+            />
+          );
+        }}
+        style={styles.list}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      {/* <TouchableOpacity activeOpacity={0.8} onLongPress={savePhoto}>
+        <Image
+          source={{
+            uri: 'https://fastly.picsum.photos/id/400/2440/1400.jpg?hmac=qSYbRbCQhkj1aMCy6AEHbsrcbXalaPHr6hPCPdxlO5o',
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity> */}
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  list: {padding: 16},
+  image: {
+    height: 120,
+    width: '33%',
+    borderRadius: 4,
+    marginRight: 4,
+    marginBottom: 4,
+  },
+});
